feb1807c18a2f0506d7b1bf0889801842edbb5f4
thuyennd21@gmail.com <dthuyen2110@gmail.com>
2024-08-31 09:10:52 +0700

    init project

---
+++ BtChuong1/bai1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string HoTen, Lopsh;
+            int i;
+            double[] A = new double[4];
+            Console.Write("Ho va ten: ");
+            HoTen = Console.ReadLine();
+            Console.Write("Lop: ");
+            Lopsh = Console.ReadLine();
+            Console.Write("Diem_QTH: ");
+            A[0]=double.Parse(Console.ReadLine());
+            Console.Write("Diem_HTTTQL: ");
+            A[1]=double.Parse(Console.ReadLine());
+            Console.Write("Diem_CSLT: ");
+            A[2]=double.Parse(Console.ReadLine());
+            Console.Write("Diem_Triet: ");
+            A[3]=double.Parse(Console.ReadLine());
+            double s=0;
+            for (i=0; i<4; i++)
+            {
+                if (A[i] < 5)
+                {
+                    s=s+1;
+                }
+                else if (A[i]>=5 && A[i]<7)
+                {
+                    s=s+2;
+                }
+                else if (A[i]>=7 && A[i]<8.5)
+                {
+                    s=s+3;
+                }
+                else
+                {
+                    s=s+4;
+                }
+            }
+            s=s/4;
+            string loai;
+            if (s>=3.6){loai="Xuat sac";}
+            else if (s>=3.2){loai="Gioi";}
+            else if (s>=2.5){loai="Kha";}
+            else if (s>=2){loai="Trung binh";}
+            else {loai="Yeu";}
+            Console.WriteLine("Sinh vien {0}, Lop {1}, Dat GPA {2}, Xep loai {3}",HoTen.ToUpper(),Lopsh,s,loai);
+        }
+    }
+}
+
+
+
+
+
+
---
+++ BtChuong1/bai2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace bai2 
+{
+    class Program
+    {
+        static void Nhap(out double a, out double b,out char ToanTu)
+        {
+        Console.Write("a= ");
+        a=Convert.ToDouble(Console.ReadLine());
+        Console.Write("b= ");
+        b=Convert.ToDouble(Console.ReadLine());
+        Console.Write("Toan tu: ");
+        ToanTu=Convert.ToChar(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            double a,b, KQ=0;
+            char ToanTu;
+            string Tieptuc;
+            while (true)
+            {
+                Nhap(out a, out b, out ToanTu);
+                Thuchien( a, b,  ToanTu, ref KQ);
+                KQua(a,b,ToanTu, KQ);
+                Console.Write("Tiep tuc:");
+                Tieptuc=Console.ReadLine();
+                if (Tieptuc=="t" | Tieptuc=="T" )
+                {
+                    break;
+                }
+            }
+        }
+        static void Thuchien(double a, double b, char ToanTu, ref double KQ)
+        {
+            KQ=0;
+            switch (ToanTu)
+            {
+                case '+':
+                    KQ=a+b;
+                    break;
+                case '-':
+                    KQ=a-b;
+                    break;
+                case '*':
+                    KQ=a*b;
+                    break;
+                case '/':
+                    if (b!=0)
+                    {
+                        KQ=a/b;
+                    }
+                    else
+                        Console.Write("Khong hop le");
+                    break;
+
+            }
+        }
+        static void KQua(double a, double b, char ToanTu,double KQ)
+        {
+            Console.WriteLine("{0}{1}{2}={3}",a,ToanTu,b,KQ);
+        }
+    }
+}
+    
+    
